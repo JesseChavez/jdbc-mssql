04b2a73bd15f82ac35c5cb9cafa029bdfce9e8a3
Jesse Chavez <jesse.chavez.r@gmail.com>
2018-02-18 17:54:47 +1100

    add jar file and run bundle install

--- lib/jdbc/mssql.rb
+++ lib/jdbc/mssql.rb
@@ -5,3 +5,9 @@ module Jdbc
     # Your code goes here...
   end
 end
+
+if RUBY_PLATFORM =~ /java/
+  require 'mssql-jdbc-6.2.2.jre8.jar'
+elsif $VERBOSE
+  warn 'jdbc-mssql must be used with JRuby only'
+end
